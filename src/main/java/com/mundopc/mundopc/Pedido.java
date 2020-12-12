822dcba35d047f97d0ece904cf309fd0fa5da0fd
OscarHurOs <oscarhurtado4446@gmail.com>
2020-12-11 22:45:55 -0500

    se modifica función de agregar computadora y mostrar pedido

--- src/main/java/com/mundopc/mundopc/Pedido.java
+++ src/main/java/com/mundopc/mundopc/Pedido.java
@@ -7,37 +7,37 @@ package com.mundopc.mundopc;
  */
 public class Pedido {
 
-    private Integer idOrden;
+    private int idPedido;
 
-    private Computadora computadora[];
+    private static int contadorPedidos;
 
-    private Integer contadorOrdenes;
+    private Computadora computadoras[];
 
-    private Integer contadorComputadoras;
+    private int contadorComputadoras;
 
-    private static final Integer MAX_COMPUTADORAS = 10;
+    private static final int MAX_COMPUTADORAS = 10;
 
     public Pedido() {
-        this.idOrden = ++contadorOrdenes;
+        this.idPedido = ++contadorPedidos;
+        this.computadoras = new Computadora[Pedido.MAX_COMPUTADORAS];
     }
 
     public void AgregarComputadora(Computadora computadora) {
-
-        if (idOrden <= MAX_COMPUTADORAS) {
-
+        if (this.contadorComputadoras <= Pedido.MAX_COMPUTADORAS) {
+            this.computadoras[this.contadorComputadoras++] = computadora;
         } else {
-            System.out.println("Ya a agregado el maximo de computadoras permitidas en un pedido");
+            System.out.println("Ya ha agregado el maximo de computadoras "
+                    + " permitidas en un pedido : " + Pedido.MAX_COMPUTADORAS);
         }
     }
 
-    public double calcularTotal() {
-        double var = 0;
+    public void mostrarPedido() {
 
-        return var;
-    }
-
-    public void mostrarOrden() {
-
+        System.out.println("Id del pedido " + idPedido);
+        System.out.println("Computadoras solicitadas : ");
+        for (int i = 0; i < this.contadorComputadoras; i++) {
+            System.out.println(computadoras[i].toString());
+        }
     }
 
 }
