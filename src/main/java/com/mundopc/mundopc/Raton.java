b46eb136be084e260d611048ea9cc97d4c8d3476
OscarHurOs <oscarhurtado4446@gmail.com>
2020-12-11 22:46:33 -0500

    se cambian tipos de variables de object a primitivas

--- src/main/java/com/mundopc/mundopc/Raton.java
+++ src/main/java/com/mundopc/mundopc/Raton.java
@@ -6,26 +6,26 @@ package com.mundopc.mundopc;
  */
 public class Raton extends DispositivoEntrada {
 
-    private Integer idRaton;
+    private int idRaton;
 
-    private static Integer contadorRatones;
+    private static int contadorRatones;
 
     public Raton(String tipoEntrada, String marca) {
         super(tipoEntrada, marca); //lo primero es apuntar al constructor de la clase padre
-        this.idRaton = ++contadorRatones;
+        this.idRaton = ++Raton.contadorRatones;
     }
 
-    public Integer getIdRaton() {
+    public int getIdRaton() {
         return idRaton;
     }
 
-    public static Integer getContadorRatones() {
+    public static int getContadorRatones() {
         return contadorRatones;
     }
 
     @Override
     public String toString() {
-        return super.toString() + "Raton{" + "idRaton=" + idRaton + '}';
+        return super.toString() + "idRaton=" + idRaton + '}';
     }
 
 }
